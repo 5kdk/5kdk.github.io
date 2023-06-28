59634b7f3ea5e0614c3d619e5c13db395e0fdefd
5kdk <5kdk.code@gmail.com>
2023-06-29 03:55:13 +0900

    fix: locale 오류 픽스

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -30,8 +30,8 @@ const config = {
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'kr',
-    locales: ['kr'],
+    defaultLocale: 'ko',
+    locales: ['ko'],
   },
 
   presets: [
