950975f23c1bb4a43e71f373d0f065ae5b32618a
5kdk <5kdk.code@gmail.com>
2023-07-03 05:43:16 +0900

    fix: url 수정으로 인한 폴더명 수정,  onBrokenLinks 옵션 다시 활성화

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   trailingSlash: false,
   deploymentBranch: 'gh-pages',
 
-  onBrokenLinks: 'warn',
+  onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful
