b2d0dc2d9f41f756fd1642d87f5d41d6637f9288
5kdk <5kdk.code@gmail.com>
2023-07-03 12:26:46 +0900

    style: hero UI 변경

--- src/components/HomepageHeader/index.js
+++ src/components/HomepageHeader/index.js
@@ -5,9 +5,9 @@ const HomepageHeader = () => {
   return (
     <header className={styles.heroBanner}>
       <div className="container">
-        <p className="text--left">Greeting</p>
         <h1 className={styles.heroTitle}>
-          안녕하세요, <code>&nbsp;" ____ "&nbsp;</code>
+          안녕하세요,&nbsp;
+          <code>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</code> 한
           <br />
           프론트엔드 개발자&nbsp;
           <strong>김동규</strong>입니다.
