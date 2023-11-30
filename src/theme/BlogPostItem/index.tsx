7b3dd7ceeff527373cd6f024edd7639c8916af8e
5kdk <5kdk.code@gmail.com>
2023-11-30 15:29:50 +0900

    fix: ts 마이그레이션 작업 (#8)

--- src/theme/BlogPostItem/index.tsx
+++ src/theme/BlogPostItem/index.tsx
@@ -1,8 +1,12 @@
 import { useBlogPost } from '@docusaurus/theme-common/internal';
 import BlogPostItem from '@theme-original/BlogPostItem';
 import GiscusComponent from '@site/src/components/GiscusComponent';
+import type BlogPostItemType from '@theme/BlogPostItem';
+import type { WrapperProps } from '@docusaurus/types';
 
-const BlogPostItemWrapper = props => {
+type Props = WrapperProps<typeof BlogPostItemType>;
+
+const BlogPostItemWrapper = (props: Props) => {
   const { metadata, isBlogPostPage } = useBlogPost();
 
   const { frontMatter } = metadata;
