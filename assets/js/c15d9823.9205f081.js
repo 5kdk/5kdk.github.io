737a5f13b966be08a5683361e68ec9f72a085230
5kdk <5kdk.code@gmail.com>
2025-04-04 05:45:59 +0900

    Deploy website - based on 61aeaa623fcc820d9512bc15047e5aefc956f7bb

---
+++ assets/js/c15d9823.9205f081.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_5kdk_blog=self.webpackChunk_5kdk_blog||[]).push([[8146],{9328:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/blog","page":1,"postsPerPage":10,"totalPages":2,"totalCount":17,"nextPage":"/blog/page/2","blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
