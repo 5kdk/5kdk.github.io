a2f8fb2c6f9cab62cbfa695874171e60c130dad2
5kdk <5kdk.code@gmail.com>
2025-12-02 17:30:46 +0900

    chore: package 의존성 업데이트

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,6 +8,11 @@ const config: Config = {
     '프론트엔드 개발자 김동규의 포트폴리오 겸 블로그입니다. 누구나 이해할 수 있는 코드, 모두가 편하게 사용하는 UX를 고민하며 성장 중입니다.',
   favicon: 'img/snoopy-spoon.png',
 
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
+
   // Set the production url of your site here
   url: 'https://5kdk.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
@@ -22,7 +27,14 @@ const config: Config = {
   deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    },
+    mermaid: true,
+  },
+  themes: ['@docusaurus/theme-mermaid'],
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -211,6 +223,7 @@ const config: Config = {
       ],
       copyright: `Copyright © ${new Date().getFullYear()} 5kdk. Built with Docusaurus.🦖`,
     },
+
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.vsDark,
